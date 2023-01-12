9ec744522f0ca81069c4ab98b61a98ae76823705
andre cerqueira <andrecerq16@gmail.com>
2023-01-12 10:57:15 +0000

    completed, missing details

--- Assets/scripts/Snowfall.cs
+++ Assets/scripts/Snowfall.cs
@@ -7,7 +7,7 @@ public class Snowfall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<ParticleSystem>().Simulate(2);
+        this.GetComponent<ParticleSystem>().Simulate(3);
         this.GetComponent<ParticleSystem>().Play();
     }
 }
--- Assets/scripts/parallax.cs
+++ Assets/scripts/parallax.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,44 @@ using UnityEngine;
 
 public class parallax : MonoBehaviour
 {
-    public PlayerMovement player;
-    public Rigidbody2D rb;
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1f;
 
-    public float speed = 3f;
+    private Transform cam;
+    private Vector3 previousCamPos;
 
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
-        rb = this.GetComponent<Rigidbody2D>();
+        previousCamPos = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Move the background in the opposite direction of the camera
-        rb.velocity = new Vector2(-player.horizontalInput * speed, rb.velocity.y);
-    }
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
 
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        previousCamPos = cam.position;
+    }
 }
